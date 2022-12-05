cee428ec17bd8e53b468d691c16e6df1e81cb96e
CDIM17 <56546970+CDIM17@users.noreply.github.com>
2022-12-04 21:35:30 -0400

    Adapter Pattern For Http Requests

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,63 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    data.results.forEach(({ name, url }) => {
+    await this.pokemonModel.deleteMany({});
+
+    /*data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
 
       const no = +segments[segments.length - 2];
 
+      await this.pokemonModel.create({ name, no });
+
+      console.log({ name, no });
+    });*/
+
+    /*const insertPromisesArray = [];
+
+    data.results.forEach(async ({ name, url }) => {
+      const segments = url.split('/');
+
+      const no = +segments[segments.length - 2];
+
+      insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+
       console.log({ name, no });
     });
 
-    return data;
+    await Promise.all(insertPromisesArray);*/
+
+    const insertPokemonArray: { name: string; no: number }[] = [];
+
+    data.results.forEach(async ({ name, url }) => {
+      const segments = url.split('/');
+
+      const no = +segments[segments.length - 2];
+
+      insertPokemonArray.push({ name, no });
+
+      //console.log({ name, no });
+    });
+
+    this.pokemonModel.insertMany(insertPokemonArray);
+
+    return 'Seed Executed';
   }
 }
